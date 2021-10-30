ebab140080121893c462730e0d059021c4b64111
riverbook708 <riverbook708@gmail.com>
2021-10-30 13:58:24 +0900

    delete dotevn

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-require('dotenv').config();
 
-console.log('start building!');
 app.use(express.static('build'));
 app.listen(PORT, () => console.log('Listening on port ', PORT));

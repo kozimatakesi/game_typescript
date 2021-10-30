40eafff33440501939e997d3810d1b1b48c01f72
riverbook708 <riverbook708@gmail.com>
2021-10-30 13:31:01 +0900

    test commit

--- server.js
+++ server.js
@@ -4,5 +4,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
+app.get('/', (req, res) => {
+  res.status(200).send({ message: 'hello, api sever!' });
+});
+
 app.use(express.static('build'));
 app.listen(PORT, () => console.log('Listening on port ', PORT));

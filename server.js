fd27dc41c07dc783f09d9a769b415a1ff58bcecb
riverbook708 <riverbook708@gmail.com>
2021-10-30 13:39:06 +0900

    add comment

--- server.js
+++ server.js
@@ -4,9 +4,6 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.status(200).send({ message: 'hello, api sever!' });
-});
-
+console.log('start building!');
 app.use(express.static('build'));
 app.listen(PORT, () => console.log('Listening on port ', PORT));

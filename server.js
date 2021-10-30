54a3456695c8f8b83dfb0f35c0b6bb1b63e54c72
riverbook708 <riverbook708@gmail.com>
2021-10-30 13:22:24 +0900

    add dotenv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+require('dotenv').config();
 
 app.use(express.static('build'));
 app.listen(PORT, () => console.log('Listening on port ', PORT));

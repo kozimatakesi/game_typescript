6dcc5a4619aeff99089eaf564130d304755299fd
riverbook708 <riverbook708@gmail.com>
2021-11-13 16:22:25 +0900

    recreate footer

--- src/components/Hooter.tsx
+++ src/components/Hooter.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import { Box, Container, Link } from '@chakra-ui/layout';
-import { AiFillTwitterCircle } from 'react-icons/ai';
+import { Box } from '@chakra-ui/layout';
 
 const Hooter: React.FC = () => {
   return (
-    <Box bottom={0} position="sticky" zIndex="sticky" h="5vh" color="white">
-      <Container>
-        <Link href="https://twitter.com/gqtwomNZddivyVq">
-          <AiFillTwitterCircle />
-        </Link>
-      </Container>
-    </Box>
+    <Box bottom={0} position="sticky" zIndex="sticky" h="5vh" color="white" />
   );
 };
 
